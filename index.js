9adba5edab7e9405da13dfb038b08cb5e7a61baf
jai2003 <91177325+jai2003@users.noreply.github.com>
2025-01-29 17:22:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ console.log('Hello, this is a ;oebfog simp;anf;aele Nodefl ;sr'r;my.js app!');
 console.log("hellowoeld");
 console.log("hellowoeld");
 console.log("hellowoeld");
-console.log("hellowoeld");
 
